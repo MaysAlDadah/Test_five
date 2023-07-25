cc3d9a083fe1bdc7d9a92f9472ee7158219735e9
Mays Akram AlDadah <maysaldadah@gmail.com>
2023-07-25 23:06:04 +0200

    first commit

---
+++ resources/views/countries/index.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    @foreach ($countries as $country)
+        <li>{{ $country->name }} (avg team size {{ $country->teams_avg_size }})</li>
+    @endforeach
+</ul>
+<ul>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,5 @@
+<ul>
+    @foreach ($tasks as $task)
+        <li>{{ $task->name }} ({{ optional($task->user->name) }})</li>
+    @endforeach
+</ul>
